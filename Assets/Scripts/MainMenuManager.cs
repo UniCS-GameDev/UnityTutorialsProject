345610965ee64f4e75f50370c120669352976e35
Calimehtar-Sael <george.reed@student.manchester.ac.uk>
2021-02-24 17:22:12 +0000

    Created my first script

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private Button startGameButton;
+    [SerializeField] private string startSceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startGameButton.onClick.AddListener(StartGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void StartGame()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(startSceneName, LoadSceneMode.Single);
+    }
+}
