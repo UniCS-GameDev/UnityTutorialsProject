aee4de079302d56275f9903084c81ef8943eebc8
Calimehtar-Sael <george.reed@student.manchester.ac.uk>
2021-03-24 18:14:09 +0000

    Finish workshop01

---
+++ Assets/Scripts/CameraTracker.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraTracker : MonoBehaviour
+{
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private Vector3 targetPositionOffset;
+
+    private void Update()
+    {
+        if(targetTransform != null)
+        {
+            transform.position = targetTransform.position + targetPositionOffset;
+        }
+    }
+}
---
+++ Assets/Scripts/Grounder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class Grounder : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        TouchedGround?.Invoke(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        LeftGround?.Invoke(other);
+    }
+
+    public event Action<Collider> TouchedGround;
+    public event Action<Collider> LeftGround;
+}
